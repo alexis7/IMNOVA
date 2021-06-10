7d5bcca974ae1144962250e46580fdc513b8a2b6
alexis7 <baironnaranjo@gmail.com>
2021-06-09 22:17:05 -0500

    Se agrega funcionalidad a consultar contratos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,22 +6,26 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './component/login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import {MatCardModule} from '@angular/material/card';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatRadioModule} from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatTableModule } from '@angular/material/table';
+import {MatIconModule} from '@angular/material/icon';
 
 
 
 //Components
 import { MatMenuModule } from '@angular/material/menu';
 import { MatSliderModule } from '@angular/material/slider';
-import {MatButtonModule} from '@angular/material/button';
-import {MatInputModule} from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 import { MenuComponent } from './component/menu/menu.component';
 import { HomeComponent } from './component/home/home.component';
 import { CreateUserComponent } from './component/create-user/create-user.component';
 import { CreateContractsComponent } from './component/create-contracts/create-contracts.component';
 import { ConsultContractsComponent } from './component/consult-contracts/consult-contracts.component';
+
+
 
 
 @NgModule({
@@ -45,7 +49,9 @@ import { ConsultContractsComponent } from './component/consult-contracts/consult
     FlexLayoutModule,
     MatCardModule,
     MatSidenavModule,
-    MatRadioModule
+    MatRadioModule,
+    MatTableModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/component/consult-contracts/consult-contracts.component.ts
+++ src/app/component/consult-contracts/consult-contracts.component.ts
@@ -5,11 +5,36 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './consult-contracts.component.html',
   styleUrls: ['./consult-contracts.component.css']
 })
+
+
 export class ConsultContractsComponent implements OnInit {
 
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'fiador'];
+  dataSource = ELEMENT_DATA;
+  clickedRows = new Set<PeriodicElement>();
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  click(){
+    console.log(this.clickedRows);
+  }
+
 }
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: string;
+  symbol: string;
+  fiador: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: '15A', weight: 'Juan Garzón', symbol: 'Carlos Arango', fiador: 'Carlos'},
+  {position: 2, name: '16A', weight: 'Felipe Arias', symbol: 'Juan Zapata', fiador: 'Carlos'},
+  {position: 3, name: '17A', weight: 'Pedro Cifuentes', symbol: 'Oscar Sanchez', fiador: 'Carlos'},
+  {position: 4, name: '18A', weight: 'Angela Arango', symbol: 'Beto Cardenas', fiador: 'Carlos'},
+  {position: 5, name: '19A', weight: 'Martin Zapata', symbol: 'Camila Cifuentes', fiador: 'Carlos'},
+];
